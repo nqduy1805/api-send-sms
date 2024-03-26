7669e8f2e1c66897c0ae1add27eeecd0181c6884
nqduy1805 <tnu1805@gmail.com>
2024-03-26 11:42:50 +0700

    u

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-function display(name){
-    console.log(name)
-    return name;
-}
-display("api-send-gmail")
+var multiply = function(num1, num2) {
+    return (BigInt(num1)*BigInt(num2)).toString()
+ };
